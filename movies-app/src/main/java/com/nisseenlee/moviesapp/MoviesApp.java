8625e13db3b0b7310bdbf83c50745af48e29143b
Lee Manalac <manalac.leemichael@gmail.com>
2023-05-03 23:28:40 +0800

    [movies-app] Refactored main app class name

--- movies-app/src/main/java/com/nisseenlee/moviesapp/MoviesApp.java
+++ movies-app/src/main/java/com/nisseenlee/moviesapp/MoviesApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoviesAppApplication {
+public class MoviesApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesAppApplication.class, args);
+		SpringApplication.run(MoviesApp.class, args);
 	}
 
 }
