0eaccc18e098786015bd22e04632e05ea5187f8d
Lee Manalac <manalac.leemichael@gmail.com>
2023-05-14 12:34:40 +0800

    [movies-app-react] Added not found

--- movies-app-react/src/App.jsx
+++ movies-app-react/src/App.jsx
@@ -7,6 +7,7 @@ import { Home } from './components/home/Home'
 import { Header } from './components/header/Header'
 import { Trailer } from './components/trailer/Trailer'
 import { Reviews } from './components/reviews/Reviews'
+import { NotFound } from './components/reviews/Reviews'
 
 function App() {
   const [movies, setMovies] = useState(0);
@@ -50,17 +51,8 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Home movies={movies} />}></Route>
             <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
-            <Route 
-              path="/Reviews/:movieId" 
-              element={
-                <Reviews 
-                  getMovieData={getMovieData}
-                  movie={movie}
-                  reviews={reviews} 
-                  setReviews={setReviews} />
-              } 
-            >
-            </Route>
+            <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+            <Route path="*" element={<NotFound/>}></Route>
           </Route>
         </Routes>
       </div>
