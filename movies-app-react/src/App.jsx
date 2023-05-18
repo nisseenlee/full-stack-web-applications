b308bc0e46ea15b9909f0b87752fa0977746e033
Lee Manalac <manalac.leemichael@gmail.com>
2023-05-18 21:49:25 +0800

    [movies-app-react] Fixed bug on import

--- movies-app-react/src/App.jsx
+++ movies-app-react/src/App.jsx
@@ -7,7 +7,7 @@ import { Home } from './components/home/Home'
 import { Header } from './components/header/Header'
 import { Trailer } from './components/trailer/Trailer'
 import { Reviews } from './components/reviews/Reviews'
-import { NotFound } from './components/reviews/Reviews'
+import { NotFound } from './components/notFound/NotFound'
 
 function App() {
   const [movies, setMovies] = useState(0);
